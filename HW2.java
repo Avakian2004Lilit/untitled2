28ad68d3b5a00d83f1da4415fc7b5e0aaf0454e6
Avakian2004Lilit <114296828+Avakian2004Lilit@users.noreply.github.com>
2022-10-09 00:57:07 +0200

    Add files via upload

---
+++ HW2.java
@@ -0,0 +1,22 @@
+package classwork5;
+
+import java.util.Scanner;
+
+public class HW2 {
+    public static void main(String[] args) {
+       int temperature1 = 100;
+       int temperature2 = 100;
+       boolean work = temperature1 > 100 && temperature2 < 100;
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println();
+    }
+      static boolean isDivideFor18And3(int number){
+        return  18 % 3 == 0;
+
+
+      }
+
+
+}
