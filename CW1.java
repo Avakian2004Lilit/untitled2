6d78c5778382c1fe2e3e461217cf2d94bb963a1a
Avakian2004Lilit <114296828+Avakian2004Lilit@users.noreply.github.com>
2022-10-10 20:52:36 +0200

    Add files via upload

---
+++ CW1.java
@@ -0,0 +1,41 @@
+package classwork6;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class CW1 {
+        public static void main(String[] args) {
+
+        boolean watch = watch();
+        System.out.println(watch 1, watch 2);
+
+    }
+
+    static boolean watch() {
+        Scanner scanner = new Scanner(System.in);
+        boolean canAge = scanner.nextBoolean();
+        boolean canAge2 = scanner.nextBoolean();
+        int watch1 = scanner.nextInt();
+        int watch2 = scanner.nextInt();
+        return canAge && canAge2 && <18, watch1 == 19 && watch2 == 7;
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
