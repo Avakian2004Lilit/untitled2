2c4253de37c32d39de532cc931eb79e1f2441d09
Avakian2004Lilit <114296828+Avakian2004Lilit@users.noreply.github.com>
2022-10-13 00:58:08 +0200

    Add files via upload

---
+++ Season.java
@@ -0,0 +1,22 @@
+package homworck1;
+
+public class Season {
+    public static void main(String[] args) {
+      int num = 4;
+      switch (num){
+          case 1:
+              System.out.println("Зима");
+              break;
+          case 2:
+              System.out.println("Весна");
+              break;
+          case 3:
+              System.out.println("Лето");
+              break;
+          case 4:
+              System.out.println("Осень");
+              break;
+
+      }
+
+    }
